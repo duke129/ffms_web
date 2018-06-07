4d246c2b2b1723242014337811e92cb08ccc0ab6
nandeeshpatil <nandeeshjob@gmail.com>
2018-06-07 17:00:17 +0530

    customer UI

--- FFMS/src/app/customer-management/customer-management.component.ts
+++ FFMS/src/app/customer-management/customer-management.component.ts
@@ -1,43 +1,113 @@
 import { Component, OnInit } from '@angular/core';
 import { DataTableResource } from '../data-table';
 import persons  from './customer-data'
+//import persons  from './customer-data';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'; 
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer-management',
   templateUrl: './customer-management.component.html',
   styleUrls: ['./customer-management.component.scss']
 })
+
+@Injectable()
 export class CustomerManagementComponent implements OnInit {
 
+  public books : any;
 
-  ngOnInit() {
-  }
-
-  itemResource = new DataTableResource(persons);
-
+  
+  observableBooks: Observable<Customer[]>
+  itemResource = new DataTableResource([]); 
+  errorMessage: String;
     items = [];
     itemCount = 0;
     selectedpersonname= '';
 
-    constructor() {
-        this.itemResource.count().then(count => this.itemCount = count);
+    constructor(private http : Http) {
+         this.itemResource.count().then(
+           count => this.itemCount = count);
+       //this.books=[new Asset("Refrigrator","Refrigrator","refrigrator")];
     }
+    
+    ngOnInit() {
 
+   ///  this.http.get('http://localhost:8080/asset/list')
+    //  .subscribe(response =>this.asset);
+    this.observableBooks = this.getBooksWithObservable();
+    this.observableBooks.subscribe(
+              result => { 
+            
+              });
+      
+      }
+
+     
+
+      getBooksWithObservable(): Observable<Customer[]> {
+        return this.http.get('http://localhost:8081/customer/getall')
+	        .map(this.extractData)
+	        .catch(this.handleErrorObservable);
+    }
+    getBooksWithPromise(): Promise<Customer[]> {
+        return this.http.get('http://localhost:8081/customer/getall').toPromise()
+	    .then(this.extractData)
+	    .catch(this.handleErrorPromise);
+    }
+    private extractData(res: Response) {
+  let book = res.json();
+        return book;
+    }
+    private handleErrorObservable (error: Response | any) {
+	console.error(error.message || error);
+	return Observable.throw(error.message || error);
+    }
+    private handleErrorPromise (error: Response | any) {
+	console.error(error.message || error);
+	return Promise.reject(error.message || error);
+    }	
+
+   // itemResource = new DataTableResource(this.books);
+    
+    
     reloadItems(params) {
-        this.itemResource.query(params).then(items => this.items = items);
+      // alert(JSON.stringify(params));
+
+      this.observableBooks = this.getBooksWithObservable();
+      this.observableBooks.subscribe(
+                result => { 
+                
+                  this.books = result ;
+                  // this.itemCount = 12;
+
+                  // alert("hi"+this.books);
+                      new DataTableResource(this.books).query(params).then(items => this.items = items);
+                });
+
+
+
+      // alert("assets :: "+this.books);
+      
     }
     addnewCustomer() {
-    let customername =  prompt("Please enter your name:", "");
-    let Emailid =  prompt("Please enter your Email id:", "");
-    let Phoneno =  prompt("Please enter your  Phone no:", "");
+    let name =  prompt("Please enter your name:", "");
+    let type =  prompt("Please enter your Email id:", "");
+    let desc =  prompt("Please enter your  Phone no:", "");
     let curentDate = new Date() + ' ';
-     const person1 = { 'name': customername, 'email': Emailid, 'jobTitle': 'Regional Configuration Producer',
-      'active': true, 'phoneNumber': Phoneno, 'date': curentDate};
+     const asset1 = { 'assetName':'assetName', ' assetDescription': 'assetDescription', 'assetTypeDes': 'assetTypeDes'};
+
+     
       let val = confirm('Do you want to add');
       if(val)
       {
-        persons.push(person1);
-        this. itemResource = new DataTableResource(persons);
+        this.books.push(this.books[0]);
+        //this. itemResource = new DataTableResource(this.books);
      alert('Added Successfully');
       }
 
---
+++ FFMS/src/app/customer-management/customer.ts
@@ -0,0 +1,16 @@
+export class Customer {
+      firstName: string;
+      lastName:string;
+      mobileNumber: string;
+//    idAsset:string;
+//    status:string;
+//   installationLat:string;
+//   installationLong:string;
+//    idAssetType:string;
+      // constructor( assetName:string,assetDescription:string,assetTypeDes:string ) { 
+      //     this.assetName=assetName;
+      //     this.assetDescription=assetDescription;
+      //     this.assetTypeDes=assetTypeDes;
+      // }
+      
+   } 
